a3dc59fb7041e830672b269c3d6b5a46eee66d5e
tommycapello <thomcapello@gmail.com>
2021-07-13 18:06:25 -0400

    seed file completed

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+        vin: "WDCGG8HB0AF462890",
+        make: 'toyota',
+        model: "prius",
+        mileage: 215000,
+        title: "clean",
+        transmission: "manual"
+    },
+    {
+        vin: "1G8MG35X48Y106575",
+        make: 'acura',
+        model: "integra",
+        mileage: 115000,
+        title: "clean"
+    },
+    {
+        vin: "1G8DC18H4CF114023",
+        make: 'chevy',
+        model: "blazer",
+        mileage: 150000,
+        title: "clean",
+        transmission: "salvage"
+    },
+]
+
+exports.seed = async function(knex){
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
